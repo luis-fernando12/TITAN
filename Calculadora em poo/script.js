235a74b25c7c11fae62bc6a117e32657aaae8541
luis-fernando <fernandobasousan@gmail.com>
2024-06-09 14:46:27 -0300

    upload calculadora

---
+++ Calculadora em poo/script.js
@@ -0,0 +1,30 @@
+class Calculadora {
+    static soma(a,b) {
+        console.log(a+b)
+    }
+    static sub(a,b) {
+        console.log(a-b)
+    }
+    static multi(a,b) {
+        console.log(a*b)
+    }
+    static divi(a,b) {
+        console.log(a/b)
+    }
+    static expo(a,b) {
+        console.log(a**b)
+    }
+    static raiz(a,b) {
+        console.log(a**1/b)
+    }
+    
+}
+
+
+Calculadora.soma(1,5)
+Calculadora.sub(20,21)
+Calculadora.multi(8,6)
+Calculadora.divi(42,7)
+Calculadora.expo(5,2)
+Calculadora.raiz(16,2)
+
